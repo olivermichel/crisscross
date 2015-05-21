559cff9a98b448d60b316ef9cd5c6ecc8ed3deca
Oliver Michel <oliver.michel@colorado.edu>
2015-05-21 14:59:03 -0600

    rubocop style fixes

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,89 +1,81 @@
-
+# represents an undirected graph
 class Graph
+  def initialize
+    @g = {}
+    @e = []
+  end
 
-	def initialize
-		@g, @e = {}, [] 
-	end
+  def edges
+    @e
+  end
 
-	def edges
-		@e
-	end
+  def vertices
+    @g.keys
+  end
 
-	def vertices
-		@g.keys
-	end
+  def add_vertex(v)
+    fail 'vertex already exists' if @g.key? v
+    @g[v] = {}
+    v.g = self
+  end
 
-	def add_vertex v
-		raise ArgumentError.new "vertex already exists" if @g.has_key? v
-		@g[v] = {}
-		v.g = self
-	end
+  def remove_vertex(v)
+    fail 'vertex does not exists' unless @g.key? v
+    @g.delete v
+  end
 
-	def remove_vertex v
-		raise ArgumentError.new "vertex does not exists" unless @g.has_key? v
-		@g.delete v
-	end
+  def add_edge(a, b, e)
+    fail 'edge already exists' if @e.include? e
+    fail 'specified vertices do not exist' unless @g.key?(a) && @g.key?(b)
 
-	def add_edge a, b, e
-		raise ArgumentError.new "edge already exists" if @e.include? e
+    @g[a][b] = e
+    @g[b][a] = e
+    e.a = a
+    e.b = b
+    @e << e
+    e.g = self
+  end
 
-		unless @g.has_key? a and @g.has_key? b
-			raise ArgumentError.new "specified vertices do not exist"
-		end
+  def remove_edge_between_vertices(a, b)
+    unless @g.key?(a) && @g.key?(b) && @g[a][b] == @g[b][a]
+      fail 'there is no edge between the specified vertices'
+    end
 
-		@g[a][b], @g[b][a], e.a, e.b = e, e, a, b
-		@e << e
-		e.g = self
-	end
+    e = @g[a][b]
+    @g[a].delete b
+    @g[b].delete a
+    @e.delete e
+  end
 
-	def remove_edge_between_vertices a, b
-
-		unless @g.has_key? a and @g.has_key? b and @g[a][b] == @g[b][a]
-			raise ArgumentError.new "there is no edge between the specified vertices"
-		end
-
-		e = @g[a][b]
-		@g[a].delete b
-		@g[b].delete a
-		@e.delete e
-	end
-
-	def remove_edge e
-		remove_edge_between_vertices e.a, e.b
-	end
-
+  def remove_edge(e)
+    remove_edge_between_vertices e.a, e.b
+  end
 end
 
 class Vertex
+  attr_accessor :g
 
-	attr_accessor :g
+  def initialize
+    @g = nil
+  end
 
-	def initialize
-		@g = nil
-	end
-
-	def neighbors
-		@g.g[self].keys
-	end
-
+  def neighbors
+    @g.g[self].keys
+  end
 end
 
 class Edge
+  attr_accessor :g, :a, :b
 
-	attr_accessor :g, :a, :b
-
-	def initialize
-		@g, @a, @b = nil, nil, nil
-	end
-
+  def initialize
+    @g = nil
+    @a = nil
+    @b = nil
+  end
 end
 
-
 class Link < Edge
-
 end
 
 class Node < Vertex
-
 end
-
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -2,124 +2,119 @@
 require 'graph'
 
 describe Graph do
+  before do
+    @graph = Graph.new
+  end
 
-	before do
-		@graph = Graph.new
-	end
+  describe '#initialize' do
+    it 'is empty upon creation' do
+      graph2 = Graph.new
+      expect(graph2.edges).to be_empty
+      expect(graph2.vertices).to be_empty
+    end
+  end
 
-	describe "#initialize" do
-		it "is empty upon creation" do
-			graph2 = Graph.new
-			expect(graph2.edges).to be_empty
-			expect(graph2.vertices).to be_empty
-		end
-	end
+  describe '#add_vertex' do
+    before do
+      @v = Vertex.new
+    end
 
-	describe "#add_vertex" do
+    it 'adds a vertex to the graph' do
+      @graph.add_vertex(@v)
+      expect(@graph.vertices).to include(@v)
+    end
 
-		before do 
-			@v = Vertex.new
-		end
+    it 'raises an error on adding an already existing vertex' do
+      @graph.add_vertex(@v)
+      expect { @graph.add_vertex(@v) }.to raise_error
+    end
+  end
 
-		it "adds a vertex to the graph" do
-			@graph.add_vertex(@v)
-			expect(@graph.vertices).to include(@v)
-		end
+  describe '#remove_vertex' do
+    before do
+      @v = Vertex.new
+    end
 
-		it "raises an error on adding an already existing vertex" do
-			@graph.add_vertex(@v)
-			expect { @graph.add_vertex(@v) }.to raise_error ArgumentError
-		end
+    it 'removes a vertex from the graph' do
+      @graph.add_vertex @v
+      @graph.remove_vertex @v
+      expect(@graph.vertices).to_not include(@v)
+    end
 
-	end
+    it 'raises an error when removing a non existing vertex' do
+      expect { @graph.remove_vertex @v }.to raise_error
+    end
+  end
 
-	describe "#add_edge" do
+  describe '#add_edge' do
+    before do
+      @a = Vertex.new
+      @b = Vertex.new
+      @e = Edge.new
+    end
 
-		before do
-			@a = Vertex.new
-			@b = Vertex.new
-			@e = Edge.new
-		end			
+    it 'adds an edge to the graph' do
+      @graph.add_vertex @a
+      @graph.add_vertex @b
+      @graph.add_edge @a, @b, @e
+      expect(@graph.edges).to include(@e)
+    end
 
-		it "adds an edge to the graph" do
-			@graph.add_vertex @a
-			@graph.add_vertex @b
-			@graph.add_edge @a, @b, @e
-			expect(@graph.edges).to include(@e)
-		end
+    it 'raises an error when adding an already existing edge' do
+      @graph.add_vertex @a
+      @graph.add_vertex @b
+      @graph.add_edge @a, @b, @e
+      expect { @graph.add_edge @a, @b, @e }.to raise_error
+    end
 
-		it "raises an error when adding an already existing edge" do
-			@graph.add_vertex @a
-			@graph.add_vertex @b
-			@graph.add_edge @a, @b, @e
-			expect { @graph.add_edge @a, @b, @e }.to raise_error ArgumentError
-		end
+    it 'raises an error when adding an edge between non existing vertices' do
+      @graph.add_vertex(@a)
+      expect { @graph.add_edge @a, @b, @e }.to raise_error
+    end
+  end
 
-		it "raises an error when adding an edge between non existing vertices" do
-			@graph.add_vertex(@a)
-			expect { @graph.add_edge @a, @b, @e }.to raise_error ArgumentError
-		end
+  describe '#remove_edge_between_vertices' do
+    before do
+      @a = Vertex.new
+      @b = Vertex.new
+      @e = Edge.new
+      @graph.add_vertex @a
+      @graph.add_vertex @b
+      @graph.add_edge @a, @b, @e
+    end
 
-	end
+    it 'removes an edge from the graph by specifying the vertices' do
+      @graph.remove_edge_between_vertices @a, @b
+      expect(@graph.edges).to_not include(@e)
+    end
 
-	describe "#remove_vertex" do
+    it 'raises an error when removing an non existing edge' do
+      c = Vertex.new
+      d = Vertex.new
+      expect do
+        @graph.remove_edge_between_vertices c, d
+      end .to raise_error
+    end
+  end
 
-		before do
-			@v = Vertex.new
-		end
+  describe '#remove_edge' do
+    before do
+      @a = Vertex.new
+      @b = Vertex.new
+      @e = Edge.new
+      @graph.add_vertex @a
+      @graph.add_vertex @b
+      @graph.add_edge @a, @b, @e
+    end
 
-		it "removes a vertex from the graph" do
-			@graph.add_vertex @v
-			@graph.remove_vertex @v
-			expect(@graph.vertices).to_not include(@v)
-		end
+    it 'removes an edge from the graph by specifying the edge' do
+      @graph.remove_edge @e
+      expect(@graph.edges).to_not include(@e)
+    end
 
-		it "raises an error when removing a non existing vertex" do
-			expect { @graph.remove_vertex @v }.to raise_error ArgumentError
-		end
-
-	end
-
-	describe "#remove_edge_between_vertices" do
-
-		before do
-			@a, @b, @e = Vertex.new, Vertex.new, Edge.new
-			@graph.add_vertex @a
-			@graph.add_vertex @b
-			@graph.add_edge @a, @b, @e
-		end
-
-		it "removes an edge from the graph by specifying the vertices" do
-			@graph.remove_edge_between_vertices @a, @b
-			expect(@graph.edges).to_not include(@e)
-		end
-
-		it "raises an error when removing an non existing edge" do
-			c, d, f = Vertex.new, Vertex.new, Edge.new
-			expect { @graph.remove_edge_between_vertices c, d }.to raise_error ArgumentError
-		end
-
-	end
-
-	describe "#remove_edge" do
-
-		before do
-			@a, @b, @e = Vertex.new, Vertex.new, Edge.new
-			@graph.add_vertex @a
-			@graph.add_vertex @b
-			@graph.add_edge @a, @b, @e
-		end
-
-		it "removes an edge from the graph by specifying the edge" do
-			@graph.remove_edge @e
-			expect(@graph.edges).to_not include(@e)
-		end
-
-		it "raises an error when removing an non existing edge" do
-			c, d, f = Vertex.new, Vertex.new, Edge.new
-			expect { @graph.remove_edge f }.to raise_error ArgumentError
-		end
-
-	end
-
+    it 'raises an error when removing an non existing edge' do
+      f = Edge.new
+      expect { @graph.remove_edge f }.to raise_error
+    end
+  end
 end
